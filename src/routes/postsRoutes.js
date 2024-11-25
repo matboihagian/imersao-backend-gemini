293ac09966c3560253f8278809b757263d2f94e3
mboihagian <mboihagian@gmail.com>
2024-11-25 15:09:20 -0300

    aula 5

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,6 +1,12 @@
 import express from "express";
 import multer from "multer";
-import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+import cors from "cors";
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postsController.js";
+
+const corsOption ={
+    origin: "http://localhost:8000",
+    optionSuccessStatus: 200
+}
 
 // Configura o armazenamento do Multer para uploads de imagens
 const storage = multer.diskStorage({
@@ -18,9 +24,11 @@ const upload = multer({ storage: storage });
 
 const routes = (app) => {
     app.use(express.json());
+    app.use(cors(corsOption));
     app.get("/posts", listarPosts);
     app.post("/posts", postarNovoPost);
-    app.post("/upload", upload.single("imagem"), uploadImagem)
+    app.post("/upload", upload.single("imagem"), uploadImagem);
+    app.put("/upload/:id", atualizarNovoPost)
 }
 
 export default routes;
