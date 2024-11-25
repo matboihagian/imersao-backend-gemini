a47040e4e1cd0992aa3b73bf013c1cd2282241a9
mboihagian <mboihagian@gmail.com>
2024-11-25 14:22:13 -0300

    aula 4 completa

--- src/models/postsModels.js
+++ src/models/postsModels.js
@@ -2,8 +2,14 @@ import conectarAoBanco from "../config/dbconfig.js";
 
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
-export default async function getTodosPosts(){
+export async function getTodosPosts(){
     const db = conexao.db("imersao-backend");
     const colecao = db.collection("posts");
     return colecao.find().toArray();
+}
+
+export async function criarPost(novoPost) {
+    const db = conexao.db("imersao-backend");
+    const colecao = db.collection("posts");
+    return colecao.insertOne(novoPost);
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,9 +1,26 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsController.js";
+import multer from "multer";
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+
+// Configura o armazenamento do Multer para uploads de imagens
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      // Especifica o diretório para armazenar as imagens enviadas
+      cb(null, 'uploads/'); // Substitua por seu caminho de upload desejado
+    },
+    filename: function (req, file, cb) {
+      // Mantém o nome original do arquivo por simplicidade
+      cb(null, file.originalname); // Considere usar uma estratégia de geração de nomes únicos para produção
+    }
+});
+  // Cria uma instância do middleware Multer
+const upload = multer({ storage: storage });
 
 const routes = (app) => {
     app.use(express.json());
     app.get("/posts", listarPosts);
+    app.post("/posts", postarNovoPost);
+    app.post("/upload", upload.single("imagem"), uploadImagem)
 }
 
 export default routes;
